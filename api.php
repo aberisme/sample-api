56e83f1a4205c5ec4bbdaa7bfab9ada8eb978d7e
Abe-r <abraham.magpantay@gmail.com>
2025-10-22 17:26:06 +0800

    Add files via upload

--- api.php
+++ api.php
@@ -10,50 +10,78 @@
     */
 
     $response = [];
+    $data['name'] = "https://www.youtube.com/@aberisme";
 
     if ($_SERVER['REQUEST_METHOD'] === 'GET') {
 
         $response = [
              "status" => "success",
-             "message" => "You issued a GET request",
+             "message" => "You issued a GET request, sample name data is given",
+             "data" => [
+                "name" => htmlspecialchars($data['name'])
+             ]
         ];
         http_response_code(200); // OK
 
     } elseif ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $data = json_decode(file_get_contents("php://input"), true);
 
-        $response = [
-             "status" => "success",
-             "message" => "You issued a POST request",
-             "data" => [
-                "name" => htmlspecialchars($data['name'])
-             ]
-        ];
-        http_response_code(200); // OK
+        if(empty($data['name'])) {
+            $response = [
+                 "status" => "error",
+                 "message" => "You must issue the POST request without name data",
+            ];
+            http_response_code(400); // Bad request
+        } else {
+            $response = [
+                 "status" => "success",
+                 "message" => "You issued a POST request with the given name data",
+                 "data" => [
+                    "name" => htmlspecialchars($data['name'])
+                 ]
+            ];
+            http_response_code(200); // OK
+        }
 
     } elseif ($_SERVER['REQUEST_METHOD'] === 'PUT') {
         $data = json_decode(file_get_contents("php://input"), true);
 
-        $response = [
-             "status" => "success",
-             "message" => "You issued a PUT request",
-             "data" => [
-                "name" => htmlspecialchars($data['name'])
-             ]
-        ];
-        http_response_code(200); // OK
+        if(empty($data['name'])) {
+            $response = [
+                 "status" => "error",
+                 "message" => "You must issue the PUT request without name data",
+            ];
+            http_response_code(400); // Bad request
+        } else {
+            $response = [
+                 "status" => "success",
+                 "message" => "You issued a PUT request with the given name data",
+                 "data" => [
+                    "name" => htmlspecialchars($data['name'])
+                 ]
+            ];
+            http_response_code(200); // OK
+        }
 
     } elseif ($_SERVER['REQUEST_METHOD'] === 'DELETE') {
         $data = json_decode(file_get_contents("php://input"), true);
 
-        $response = [
-             "status" => "success",
-             "message" => "You issued a DELETE request",
-             "data" => [
-                "name" => htmlspecialchars($data['name'])
-             ]
-        ];
-        http_response_code(200); // OK
+        if(empty($data['name'])) {
+            $response = [
+                 "status" => "error",
+                 "message" => "You must issue the DELETE request without name data",
+            ];
+            http_response_code(400); // Bad request
+        } else {
+            $response = [
+                 "status" => "success",
+                 "message" => "You issued a DELETE request on the given name data",
+                 "data" => [
+                    "name" => htmlspecialchars($data['name'])
+                 ]
+            ];
+            http_response_code(200); // OK
+        }
 
     } else {
         $response = [
